52b46f2da23ffc7daae36d75830574de65cd8c9b
Aleksey Popov <alepooop@gmail.com>
2021-03-27 01:46:58 +0300

    migrate to null safety

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 2.0.0 (2021-03-27)
+- migrate to null safety
+
 ## 1.1.0 (2021-02-14)
 
 - fix typo into getPublicKey method
--- lib/src/hd_key.dart
+++ lib/src/hd_key.dart
@@ -16,40 +16,48 @@ class _ED25519HD {
 
   const _ED25519HD();
 
-  KeyData derivePath(String path, Uint8List seedBytes,
-      {int offset = HARDENED_OFFSET}) {
-
+  Future<KeyData> derivePath(String path, List<int> seedBytes,
+      {int offset = HARDENED_OFFSET}) async {
     if (!_ED25519HD._pathRegex.hasMatch(path)) {
       throw ArgumentError(
           "Invalid derivation path. Expected BIP32 path format");
     }
 
-    KeyData master = getMasterKeyFromSeed(seedBytes);
+    KeyData master = await getMasterKeyFromSeed(seedBytes);
+
     List<String> segments = path.split('/');
     segments = segments.sublist(1);
 
-    return segments.fold<KeyData>(master, (prevKeyData, indexStr) {
-      int index = int.parse(indexStr.substring(0, indexStr.length - 1));
-      return _getCKDPriv(prevKeyData, index + offset);
-    });
+    KeyData result = master;
+
+    for (String segment in segments) {
+      int index = int.parse(segment.substring(0, segment.length - 1));
+      result = await _getCKDPriv(result, index + offset);
+    }
+
+    return result;
   }
 
-  Uint8List getPublicKey(Uint8List privateKey, [bool withZeroByte = true]) {
-    final signature = ed25519.newKeyPairFromSeedSync(PrivateKey(privateKey));
+  Future<KeyData> getMasterKeyFromSeed(List<int> seedBytes) =>
+      _getKeys(seedBytes, _ED25519HD._curveBytes);
+
+  Future<List<int>> getPublicKey(List<int> privateKey,
+      [bool withZeroByte = true]) async {
+    final algorithm = Ed25519();
+    final signature = await algorithm.newKeyPairFromSeed(privateKey);
+    final publicKey = await signature.extractPublicKey();
+
     if (withZeroByte == true) {
-      Uint8List dataBytes = Uint8List(33);
+      List<int> dataBytes = List.filled(33, 0);
       dataBytes[0] = 0x00;
-      dataBytes.setRange(1, 33, signature.publicKey.bytes);
+      dataBytes.setRange(1, 33, publicKey.bytes);
       return dataBytes;
     } else {
-      return signature.publicKey.bytes;
+      return publicKey.bytes;
     }
   }
 
-  KeyData getMasterKeyFromSeed(Uint8List seedBytes) =>
-      _getKeys(seedBytes, _ED25519HD._curveBytes);
-
-  KeyData _getCKDPriv(KeyData data, int index) {
+  Future<KeyData> _getCKDPriv(KeyData data, int index) {
     Uint8List dataBytes = Uint8List(37);
     dataBytes[0] = 0x00;
     dataBytes.setRange(1, 33, data.key);
@@ -57,13 +65,16 @@ class _ED25519HD {
     return _getKeys(dataBytes, data.chainCode);
   }
 
-  KeyData _getKeys(Uint8List data, Uint8List keyParameter) {
-    final hmac = Hmac(sha512).newSink(secretKey: SecretKey(keyParameter));
-    hmac
+  Future<KeyData> _getKeys(List<int> data, List<int> keyParameter) async {
+    final hmac = Hmac.sha512();
+    final sink = await hmac.newMacSink(secretKey: SecretKey(keyParameter));
+    sink
       ..add(data)
       ..close();
 
-    final I = hmac.mac.bytes;
+    final mac = await sink.mac();
+
+    final I = mac.bytes;
     final IL = I.sublist(0, 32);
     final IR = I.sublist(32);
 
